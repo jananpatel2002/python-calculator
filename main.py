d51da07778a439b76aa9077dd84fce2f305240dc
jananpatel2002 <70957608+jananpatel2002@users.noreply.github.com>
2022-10-20 04:18:27 +0000

    Working/Recursion used instead of While(True) loop

--- main.py
+++ main.py
@@ -22,26 +22,36 @@ operations = {
   "*":multiply
 }
 
+def calculator():
+  shouldContinue = True
+
+  n1 = float(input("What is your first number? "))
+
+  while(shouldContinue):
+    
+    for symbol in operations:
+      print(symbol)
+    
+    symbol = input("Select any operation from above. ")
+    while symbol not in operations:
+      symbol = input("Please select a valid symbol from above: ")
+    n2 = float(input("What is your second number? "))
+    answer = operations[symbol](n1,n2) # Really simple code. Functions are stored above and then are accessed based on the input
+    print(f"{n1} {symbol} {n2} = {answer}")
+    
+    if (input(f"Use {answer} in next calculation? Yes or No: ").lower() =='yes'):
+      n1 = answer
+    else:
+      shouldContinue = False
+      clear()
+      calculator()
+
 useAnswer = 'no'
+
 ### MAIN CODE ###
-while(True):
-  clear()
-  if(useAnswer == 'yes'):
-    n1 = answer
-  else:
-    n1 = int(input("What is your first number? "))
-  
-  for symbol in operations:
-    print(symbol)
-  
-  symbol = input("Select any operation from above. ")
-  while symbol not in operations:
-    symbol = input("Please select a valid symbol from above: ")
-  n2 = int(input("What is your second number? "))
-  answer = operations[symbol](n1,n2) # Really simple code. Functions are stored above and then are accessed based on the input
-  print(f"{n1} {symbol} {n2} = {answer}")
-  
-  useAnswer= input("Would you like to use this Answer for the next calculation? Yes or No: ").lower()
+
+
+calculator()
 
 
   
